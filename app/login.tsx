4b0528ab015d3af3163ceb33ac2a593ec2a61e2e
rbrasildev <rbrasildev@gmail.com>
2024-08-26 22:11:32 -0400

    flatlist

--- app/login.tsx
+++ app/login.tsx
@@ -1,4 +1,5 @@
 import api from '@/services/api'
+import { MaterialCommunityIcons } from '@expo/vector-icons'
 import { useAsyncStorage } from '@react-native-async-storage/async-storage'
 import { useState } from 'react'
 import { Text, View, TextInput, TouchableOpacity, FlatList } from 'react-native'
@@ -8,33 +9,28 @@ const Login = () => {
     const [cpfCnpj, setCpfCnpj] = useState('95231447234')
     const [contratoData, setContratoData] = useState([])
 
-    const { setItem } = useAsyncStorage('@sgp')
+    const { setItem, getItem } = useAsyncStorage('@sgp')
 
     const handleGetDataUser = async () => {
         try {
             const response = await api(cpfCnpj, '123456')
             setContratoData(response)
+            handeSaveData()
+
         } catch (error) {
             console.log(error)
         }
     }
 
-    const handeSaveData = async () => {
-        const newData = {
-            contrato,
-            cpfcnpj,
 
-        }
+    const handeSaveData = async () => {
+        setItem(JSON.stringify(contratoData));
         try {
 
         } catch (error) {
             console.log(error)
         }
     }
-
-    console.log(contratoData.auth)
-
-
 
     return (
         <View
@@ -46,10 +42,10 @@ const Login = () => {
                 paddingTop: 30,
             }}
         >
-            <Text style={{ color: '#fff' }}>PÁGINA DE LOGIN</Text>
+            <Text style={{ color: '#fff' }}>Login</Text>
             <TextInput
                 placeholder='Digite CPF/CNPJ'
-                placeholderTextColor={'#666'}
+                placeholderTextColor={'#999'}
                 onChangeText={setCpfCnpj}
                 value={cpfCnpj}
                 style={{
@@ -57,7 +53,7 @@ const Login = () => {
                     padding: 16,
                     borderRadius: 16,
                     marginVertical: 16,
-                    color: '#666'
+                    color: '#999'
                 }}
             />
             <TouchableOpacity
@@ -67,24 +63,42 @@ const Login = () => {
                     padding: 20,
                     borderRadius: 16,
                     justifyContent: 'center',
-                    alignItems: 'center'
+                    alignItems: 'center',
+                    marginBottom: 16
                 }}
             >
-                <Text style={{ color: '#fff' }}>Entrar</Text>
+                <Text style={{ color: '#fff', fontSize: 18 }}>Entrar</Text>
             </TouchableOpacity>
 
             <FlatList
                 data={contratoData.contratos}
                 renderItem={({ item }) => (
-                    <View style={{ flexDirection: 'row', gap: 4, justifyContent: 'space-between', backgroundColor: '#333', marginVertical: 2, padding: 10, borderRadius: 10 }}>
-                        <Text style={{ color: '#fff' }}>{item.contrato}</Text>
-                        <Text style={{ color: '#fff' }}>{item.razaosocial}</Text>
+                    <View>
+                        <TouchableOpacity
+                            style={{
+                                flexDirection: 'row',
+                                gap: 4, justifyContent: 'space-around',
+                                alignItems: 'center',
+                                backgroundColor: '#333',
+                                marginVertical: 2,
+                                padding: 10,
+                                borderRadius: 16,
+                                borderWidth: 1,
+                                borderColor: "#666",
+                            }}>
+                            <View style={{ flexDirection: 'row' }}>
+                                <MaterialCommunityIcons size={20} color={'#666'} name='file-account' />
+                                <Text style={{ color: '#fff' }}>{item.contrato}</Text>
+                            </View>
+                            <View>
+                                <Text style={{ color: '#fff', fontWeight: '700' }}>{item.razaosocial}</Text>
+                                <Text style={{ color: '#666' }}>{item.planointernet}</Text>
+                            </View>
+                        </TouchableOpacity>
                     </View>
                 )}
                 keyExtractor={item => item.contrato}
             />
-
-
         </View>
     )
 }
